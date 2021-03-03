10589eb100fffde3f7dff277d8b8fcd4a49337b5
rudradesai200 <rudradesai200@gmail.com>
2021-03-03 20:08:57 +0530

    Adding comments and refactoring

--- Assignment1/emailserver.c
+++ Assignment1/emailserver.c
@@ -11,7 +11,6 @@
 #define USERIDLIMIT 100
 
 // Data Structures
-
 typedef struct Email
 {
     struct User *from, *to;
@@ -33,6 +32,8 @@ int new_socket;
 User *users[NUSERS], *current_user;
 
 // Helper functions
+
+/** Appends cnt number of strings and returns a final string.*/
 char *str_append(int cnt, char *str, ...)
 {
     char *retstr;
@@ -47,25 +48,24 @@ char *str_append(int cnt, char *str, ...)
     va_end(arg);
     return retstr;
 }
+
+/** Returns 'mail_root/users/uid.txt' */
 char *get_filename(char *uid) { return str_append(3, "mail_root/users/", uid, ".txt"); }
 
-ssize_t store_email(struct Email *email, FILE *fp) { return fwrite(email, sizeof(struct Email), 1, fp); }
-
-ssize_t read_email(FILE *fp, struct Email *inp) { return fread(inp, sizeof(struct Email), 1, fp); }
-
+/** Converts email to string in a specified format */
 char *email_to_str(struct Email *email)
 {
-    char *data;
-    data = calloc(BUFSIZ, sizeof(char));
+    char *data = calloc(BUFSIZ, sizeof(char));
     strcpy(data, str_append(3, "From: ", email->from->userid, "\n"));
     strcat(data, str_append(3, "To: ", email->to->userid, "\n"));
     strcat(data, str_append(3, "Date: ", email->dt, "\n"));
-    // strcat(data, str_append(3, "Subject: ", email->subject, "\n"));
     strcat(data, str_append(3, "Contents:\n", email->content, "###\n"));
-    // printf("EMAIL : %s\n", data);
     return data;
 }
 
+/** Returns the useridx of the user stored in users array.
+ * If not present returns -1.
+ */
 int isUserPresent(char *userid)
 {
     for (int i = 0; i < current_users; i++)
@@ -74,6 +74,7 @@ int isUserPresent(char *userid)
     return -1;
 }
 
+/** Reads an email from the passed fp  */
 Email *read_email_from_file(FILE *fp)
 {
     if (fp == NULL)
@@ -85,18 +86,26 @@ Email *read_email_from_file(FILE *fp)
         int read;
         size_t len = 0;
         Email *em = calloc(1, sizeof(Email));
-        em->dt = calloc(BUFSIZ, sizeof(char));
+
+        // Check the first line. Return NULL if not present
         if (fscanf(fp, "From: %s\n", tstr) != 1)
-        {
             return NULL;
-        }
         em->from = users[isUserPresent(tstr)];
+
+        // Read recipient of the email
         fscanf(fp, "To: %s\n", tstr);
         em->to = users[isUserPresent(tstr)];
+
+        // Read date of the email
         fgets(tstr, BUFSIZ * sizeof(char), fp);
+        em->dt = calloc(BUFSIZ, sizeof(char));
         strcpy(em->dt, &tstr[6]);
         em->dt[strlen(em->dt) - 1] = '\0';
+
+        // Ignore content: header
         fscanf(fp, "%s\n", tstr);
+
+        // Read content lines
         while ((read = getline(&tstr, &len, fp)) != -1)
         {
             if (strcmp(tstr, "###\n") == 0)
@@ -105,22 +114,24 @@ Email *read_email_from_file(FILE *fp)
         }
         em->content = calloc(BUFSIZ, sizeof(char));
         strcpy(em->content, content);
+
         return em;
     }
 }
 
+/** Fetch emails for the current_user from its spool file.
+ * Uses the read_email_from_file function.
+*/
 void fetch_emails()
 {
     FILE *fp = fopen(get_filename(current_user->userid), "r");
-    char *tstr = calloc(BUF_SIZE, sizeof(char)), *content = calloc(BUF_SIZE, sizeof(char));
-    ssize_t read;
-    size_t len = 0;
     Email *curr = NULL, *email = NULL;
     int i = 0;
 
     current_user->current_email = NULL;
     while ((email = read_email_from_file(fp)) != NULL)
     {
+        // Join links for this email to the next one.
         if (curr != NULL)
             curr->next = email;
         email->last = curr;
@@ -129,28 +140,26 @@ void fetch_emails()
             current_user->email_head = curr;
         i++;
     }
+
+    // Join the list with the head
     if (curr != NULL)
     {
         (curr->next) = (current_user->email_head);
         (current_user->email_head->last) = curr;
     }
+
+    // Set the current_email to the head
     current_user->current_email = current_user->email_head;
     fclose(fp);
 }
 
+/** Stores the whole user object in the fp */
 ssize_t store_user(struct User *user, FILE *fp) { return fwrite(user, sizeof(struct User), 1, fp); }
 
+/** Reads the whole user object from the fp */
 ssize_t read_user(FILE *fp, struct User *inp) { return fread(inp, sizeof(struct User), 1, fp); }
 
-void print_user(struct User *usr)
-{
-    printf("---------------------------\n");
-    printf("Userid : %s\n", usr->userid);
-    printf("Filename : %s\n", get_filename(usr->userid));
-    printf("Nmsgs : %d\n", usr->nmsgs);
-    printf("---------------------------\n");
-}
-
+/** Creates a user and add it in the users array */
 int addUser(char *userid)
 {
     if (isUserPresent(userid) != -1)
@@ -164,6 +173,7 @@ int addUser(char *userid)
     return 0;
 }
 
+/** Returns a space separated list of userids */
 char *list_users()
 {
     char *userslist = calloc(BUF_SIZE, sizeof(char));
@@ -175,6 +185,7 @@ char *list_users()
     return userslist;
 }
 
+/** initializes the users array from the mail_root/users.txt file */
 void initialize()
 {
     FILE *fp = fopen("mail_root/users.txt", "r");
@@ -185,11 +196,11 @@ void initialize()
         memcpy(users[current_users], &inp, sizeof(struct User));
         users[current_users]->current_email = NULL;
         users[current_users++]->email_head = NULL;
-        // print_user(users[current_users - 1]);
     }
     fclose(fp);
 }
 
+/** Writes all the emails back for the usr to it's own spool file */
 void write_spool_back(User *usr)
 {
     if (usr->email_head != NULL)
@@ -198,6 +209,7 @@ void write_spool_back(User *usr)
         fclose(fopen(get_filename(usr->userid), "w"));
         FILE *fp = fopen(get_filename(usr->userid), "w");
         char *str = email_to_str(usr->email_head);
+
         fwrite(str, 1, strlen(str), fp);
         while ((curr != usr->email_head))
         {
@@ -208,21 +220,24 @@ void write_spool_back(User *usr)
         fclose(fp);
     }
     else
-    {
         fclose(fopen(get_filename(usr->userid), "w"));
-    }
 }
+
+/** Stores all the users and their spool files. */
 void storeback()
 {
-    FILE *fp = fopen("users.txt", "w");
+    FILE *fp;
     for (int i = 0; i < current_users; i++)
     {
         write_spool_back(users[i]);
+        fp = fopen("users.txt", "w+");
         store_user(users[i], fp);
+        fclose(fp);
     }
     fclose(fp);
 }
 
+/** Receives command and writes it in the buffer. */
 int get_command(char *buffer, int to_print)
 {
     memset(buffer, 0, BUF_SIZE * sizeof(char));
@@ -240,6 +255,74 @@ int get_command(char *buffer, int to_print)
         }
     }
     return 0;
+}
+
+void delete_mail()
+{
+    if (current_user->current_email == current_user->current_email->next)
+    {
+        current_user->current_email = NULL;
+        current_user->email_head = NULL;
+    }
+    else
+    {
+        Email *curr = current_user->current_email;
+        curr->last->next = curr->next;
+        curr->next->last = curr->last;
+        if (current_user->current_email == current_user->email_head)
+            current_user->email_head = current_user->email_head->next;
+        current_user->current_email = current_user->current_email->next;
+        free(curr);
+    }
+    current_user->nmsgs--;
+}
+
+/** SEND function */
+Email *SEND(int toidx)
+{
+    Email *em = calloc(1, sizeof(Email));
+    em->from = current_user;
+    em->to = users[toidx];
+    char *buffer = calloc(BUF_SIZE, sizeof(char));
+    em->content = calloc(BUF_SIZE, sizeof(char));
+    while (1)
+    {
+        get_command(buffer, -1);
+        if (strcmp(&buffer[strlen(buffer) - 4], "###\n") == 0)
+        {
+            strncat(em->content, buffer, strlen(buffer) - 4);
+            strcat(em->content, "\n");
+            break;
+        }
+        else
+        {
+            strcat(em->content, buffer);
+        }
+    }
+    // strcpy(em->dt, "Today");
+    if (em->to->email_head != NULL)
+    {
+        em->last = em->to->email_head->last;
+        em->next = em->to->email_head;
+        em->to->email_head->last->next = em;
+        em->to->email_head->last = em;
+        em->to->email_head = em;
+    }
+    else
+    {
+        em->next = em;
+        em->last = em;
+        em->to->email_head = em;
+    }
+    em->to->nmsgs++;
+
+    time_t curtime;
+    time(&curtime);
+    char *dstr = ctime(&curtime);
+    dstr[strlen(dstr) - 1] = '\0';
+    em->dt = calloc(BUF_SIZE, sizeof(char));
+    strcpy(em->dt, dstr);
+    return em;
 }
 
 void command_processor(char *str)
@@ -304,22 +387,7 @@ void command_processor(char *str)
             strcpy(str, "OK\nNo More Email\n");
         else
         {
-            if (current_user->current_email == current_user->current_email->next)
-            {
-                current_user->current_email = NULL;
-                current_user->email_head = NULL;
-            }
-            else
-            {
-                Email *curr = current_user->current_email;
-                curr->last->next = curr->next;
-                curr->next->last = curr->last;
-                if (current_user->current_email == current_user->email_head)
-                    current_user->email_head = current_user->email_head->next;
-                current_user->current_email = current_user->current_email->next;
-                free(curr);
-            }
-            current_user->nmsgs--;
+            delete_mail();
             strcpy(str, "OK\n");
         }
     }
@@ -332,49 +400,7 @@ void command_processor(char *str)
             strcpy(str, str_append(3, "Not OK : No userid = ", to, "\n"));
         else
         {
-            Email *em = calloc(1, sizeof(Email));
-            em->from = current_user;
-            em->to = users[toidx];
-            char *buffer = calloc(BUF_SIZE, sizeof(char));
-            em->content = calloc(BUF_SIZE, sizeof(char));
-            while (1)
-            {
-                get_command(buffer, -1);
-                if (strcmp(&buffer[strlen(buffer) - 4], "###\n") == 0)
-                {
-                    strncat(em->content, buffer, strlen(buffer) - 4);
-                    strcat(em->content, "\n");
-                    break;
-                }
-                else
-                {
-                    strcat(em->content, buffer);
-                }
-            }
-            // strcpy(em->dt, "Today");
-            if (em->to->email_head != NULL)
-            {
-                em->last = em->to->email_head->last;
-                em->next = em->to->email_head;
-                em->to->email_head->last->next = em;
-                em->to->email_head->last = em;
-                em->to->email_head = em;
-            }
-            else
-            {
-                em->next = em;
-                em->last = em;
-                em->to->email_head = em;
-            }
-            em->to->nmsgs++;
-
-            time_t curtime;
-            time(&curtime);
-            char *dstr = ctime(&curtime);
-            str[strlen(dstr) - 1] = '\0';
-            em->dt = calloc(BUF_SIZE, sizeof(char));
-            strcpy(em->dt, dstr);
-
+            Email *em = SEND(toidx);
             strcpy(str, "OK\n");
             write_spool_back(em->to);
         }
